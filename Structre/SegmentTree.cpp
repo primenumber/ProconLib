6a79451e8ed91ab42ee17588d5c11f8855a7fca0
asi1024 <asi1024@dhcp188.box2.kmc.gr.jp>
2013-12-14 16:07:20 +0900

    add SegmentTree.cpp

---
+++ Structre/SegmentTree.cpp
@@ -0,0 +1,49 @@
+
+struct Data {
+  int num;
+  Data() : num(0) {;}
+  Data(int n) : num(n) {;}
+};
+
+inline Data Merge(Data left, Data right) {
+  return Data(left.num < right.num ? left.num : right.num);
+}
+
+struct SegmentTree {
+  static const int STsize = 1 << 18;
+  Data data[STsize]; int n;
+  SegmentTree(void) { n = STsize / 2; }
+  void update (int pos, Data value) {
+    data[pos] = value;
+    while (pos < 2*n-1) {
+      int l = pos, r = pos^1;
+      pos = pos / 2 + n;
+      data[pos] = Merge(data[l], data[r]);
+    }
+  }
+  Data sub(int fr, int to, int node, int la, int ra) {
+    if (ra<=fr || to<=la) return Data();
+    if (fr<=la && ra<=to) return data[node];
+    Data vl = sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
+    Data vr = sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
+    return Merge(vl, vr);
+  }
+  Data query(int fr, int to) { return sub(fr, to, 2*n-2, 0, n); }
+};
+
+/*
+int main() {
+  int n, q, a, b;
+  cin >> n >> q;
+  SegmentTree rmq;
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+    rmq.update(i, Data(a));
+  }
+  for (int i = 0; i < q; i++){
+    cin >> a >> b;
+    cout << rmq.query(a, b).num << endl;
+  }
+  return 0;
+}
+*/
