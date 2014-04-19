6094114da61a3b3714741c698720bfd944aebb64
Kazuki Takise <kazuki.takise@gmail.com>
2014-04-19 15:16:27 +0900

    update LCA Algorithm

---
+++ Graph/LCA(RMQ).cpp
@@ -0,0 +1,32 @@
+//==============================
+#define MAX_V 100
+int root, V;
+//==============================
+
+vector<int> g[MAX_V];
+int vs[2 * MAX_V - 1];
+int depth[2 * MAX_V - 1];
+int id[MAX_V];
+
+void dfs(int v, int p, int d, int &k) {
+  id[v] = k;
+  vs[k] = v;
+  depth[k++] = d;
+  REP(i, g.size()) {
+    if (g[v][i] != p) {
+      dfs(g[v][i], v, d + 1, k);
+      vs[k] = v;
+      depth[k++] = d;
+    }
+  }
+}
+
+void init(void) {
+  int k = 0;
+  dfs(root, -1, 0, k);
+  rmq_init(depth, 2 * V - 1);
+}
+
+int lca(int u, int v) {
+  return vs[query(min(id[u], id[v]) , max(id[u], id[v]) + 1)];
+}
