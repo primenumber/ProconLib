35adae8cad3a83fcd61db706b19a29cdd86f4183
primenumber <primenumber.proj@gmail.com>
2014-07-05 00:35:34 +0900

    fix bugs

--- Structre/SegmentTree.cpp
+++ Structre/SegmentTree.cpp
@@ -38,16 +38,33 @@ struct SegTreeLazy {
   static const int STsize = 1 << MAX_DEPTH;
   Data data[STsize];
   Data lazy[STsize]; int n;
+  SegTreeLazy(void) : n(STsize / 2) {
+    REP(i,STsize){
+      data[i]=lazy[i]=0;
+    }
+  }
   void add(int fr, int to, Data val) { upd_sub(fr, to, 2*n-2, 0, n, val); }
   Data sum(int fr, int to) { sum_sub(fr, to, 2*n-2, 0, n); }
  private:
+  void update (int pos, Data value) {
+    data[pos] += value;
+    while (pos < 2*n-1) {
+      pos = pos / 2 + n;
+      data[pos] += value;
+    }
+  }
   void upd_sub(int fr, int to, int node, int la, int ra, Data val) {
     if (ra<=fr || to<=la) return;
     if (fr<=la && ra<=to) {
-      lazy[node] += val; return;
+      update(node, val*(ra-la));
+      if(node>=n){
+        lazy[(node-n)*2+0] += val;
+        lazy[(node-n)*2+1] += val;
+      }
+      return;
     }
-    upd_sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
-    upd_sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
+    upd_sub(fr, to, (node-n)*2+0, la, (la+ra)/2, val);
+    upd_sub(fr, to, (node-n)*2+1, (la+ra)/2, ra, val);
   }
   Data sum_sub(int fr, int to, int node, int la, int ra) {
     if (ra<=fr || to<=la) return Data();
@@ -57,7 +74,7 @@ struct SegTreeLazy {
     data[node] += lazy[node]*(ra-la);
     lazy[node] = 0;
     Data vl = sum_sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
-    Data vr = sum_sub(fr, to, (node-n)*2+1, la, (la+ra)/2);
+    Data vr = sum_sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
     return vl+vr;
   }
 };
