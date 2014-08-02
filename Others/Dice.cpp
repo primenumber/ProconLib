6b69615956b21a6fac541cfaec5f22ca1e44fd4f
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-08-03 07:39:51 +0900

    Dice

--- Others/Dice.cpp
+++ Others/Dice.cpp
@@ -22,3 +22,33 @@ struct Dice{
     return 0;
   }
 };
+
+struct Dice{
+  int t, f, r, face[6];
+  Dice(void) { t = 0; f = 1; r = 2; }
+  int top() { return face[t]; }
+  int bottom() { return face[5 - t]; }
+  int right() { return face[r]; }
+  int left() { return face[5 - r]; }
+  int front() { return face[f]; }
+  int back() { return face[5 - f]; }
+  void toFront() { swap(f, t); t = 5 - t; }
+  void toBack()  { swap(f, t); f = 5 - f; }
+  void toRight() { swap(t, r); t = 5 - t; }
+  void toLeft()  { swap(t, r); r = 5 - r; }
+  void toClock() { swap(f, r); r = 5 - r; }
+  void toConter(){ swap(f, r); f = 5 - f; }
+  vector<int> toVector() {
+    return {face[t], face[f], face[r], face[5-r], face[5-f], face[5-t]};
+  }
+  vector<int> toVector2() {
+    vector<int> res = {999};
+    REP(i,30) {
+      if (i % 10 == 0) toRight();
+      else if (i % 10 == 5) toFront();
+      else toClock();
+      res = min(res, toVector());
+    }
+    return res;
+  }
+};
