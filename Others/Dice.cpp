23793c50d0618a8cbf26f22965f9cfbe0d86b703
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-08-02 10:08:34 +0900

    Fix dice

--- Others/Dice.cpp
+++ Others/Dice.cpp
@@ -1,5 +1,4 @@
-class Dice{
-public:
+struct Dice{
   int t, r, f;
   int top() { return t; }
   int bottom() { return 7 - t; }
@@ -9,6 +8,17 @@ public:
   int back() { return 7 - f; }
   void toFront() { swap(f, t); t = 7 - t; }
   void toBack() { toFront(); toFront(); toFront(); }
-  void toRight() { swap(f, r); t = 7 - t; }
+  void toRight() { swap(t, r); t = 7 - t; }
   void toLeft() { toRight(); toRight(); toRight(); }
+  void toClock() { swap(f, r); r = 7 - r; }
+  void toConter() { toClock(); toClock(); toClock(); }
+  int roll(int tt, int ff) {
+    REP(i,30) {
+      if (i % 10 == 0) toRight();
+      else if (i % 10 == 5) toFront();
+      else toClock();
+      if (tt == t && ff == f) return r;
+    }
+    return 0;
+  }
 };
