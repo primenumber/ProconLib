d5c762aaf2fe273dd5660884d695e3090413035e
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-08-12 23:53:02 +0900

    DAGを生成する関数を作成 バグの修正 Range-Based-forへの書き換え 使い方をtexに記述

--- Graph/StronglyConnectedComponents.cpp
+++ Graph/StronglyConnectedComponents.cpp
@@ -1,36 +1,40 @@
-int V;
-#define MAX_V 1000
+#define MAX_V 1024
 
-vector<int> g[MAX_V];
-vector<int> rg[MAX_V];
-vector<int> vs;
+vector<int> g[MAX_V], rg[MAX_V], vs;
 bool used[MAX_V];
 int cmp[MAX_V];
 
-boid add_edge(int from, int to) {
-  g[from].pb(to);
-  rg[to].pb(from);
+void add_edge(int from, int to) {
+  g[from].push_back(to);
+  rg[to].push_back(from);
 }
 
 void dfs(int v) {
   used[v] = true;
-  REP(i, g[v].size()) if (!used[g[v][i]]) dfs(g[v][i]);
-  vs.pb(v);
+  for (int i : g[v]) if (!used[i]) dfs(i);
+  vs.push_back(v);
 }
 
 void rdfs(int v, int k) {
-  used[v] = true;
-  cmp[v] = k;
-  REP(i, rg[v].size()) if (!used[g[v][i]]) rdfs(rg[v][i], k);
+  used[v] = true; cmp[v] = k;
+  for (int i : rg[v]) if (!used[i]) rdfs(i, k);
 }
 
-int scc(void) {
+int scc(int V) {
   memset(used, 0, sizeof(used));
   vs.clear();
   REP(v, V) if (!used[v]) dfs(v);
   memset(used, 0, sizeof(used));
+  reverse(ALL(vs));
   int k = 0;
-  for (int i = vs.size() - 1; i >= 0; i--) if (!used[vs[i]]) rdfs(vs[i], k++);
+  for (int i : vs) if (!used[i]) rdfs(i, k++);
   return k;
 }
 
+vector<vector<int>> buildGraph(int V, int K) {
+  vector<set<int>> s(K);
+  vector<vector<int>> res(K);
+  REP(i,V) for (int j : g[i]) s[cmp[i]].insert(cmp[j]);
+  REP(i,K) for (int j : s[i]) if (i != j) res[i].push_back(j);
+  return res;
+}
