fdb89647c53e3d349efa748c79df1efcafcb9f58
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-08-13 22:25:52 +0900

    球と直線の交点(三次元幾何)を追加

--- Geometry/3DGeometry.cpp
+++ Geometry/3DGeometry.cpp
@@ -23,6 +23,7 @@ ld norm(const Point &a) { return dot(a, a); }
 ld dist(const Point &a) { return sqrt(norm(a)); }
 
 struct Line { Point a, b; };
+struct Sphere { Point p; ld r; };
 
 Point project_lp(Line l, Point p) {
   Point point = l.a;
@@ -42,6 +43,7 @@ ld distance_sp(Line l, Point p) {
     return min(dist(p - l.a), dist(p - l.b));
 }
 
+// Verified : AOJ1331
 ld distance_ll (Line l, Line m) {
   Point p1 = l.a, v1 = l.b - l.a,p2 = m.a, v2 = m.b - m.a;
   Point a = p1 - p2 - dot(p1-p2, v2) / norm(v2) * v2;
@@ -49,4 +51,18 @@ ld distance_ll (Line l, Line m) {
   if (norm(b) < eps) return dist(a); // parallel
   ld t = dot(-a, b) / norm(b);
   return dist(a + t * b);
+}
+
+// Verified : AOJ1289
+vector<Point> intersect_lo(Sphere s, Line l) {
+  vector<Point> res;
+  Point p = project_lp(l, s.p);
+  ld d = dist(p - s.p);
+  if (d < s.r + eps){
+    ld len = (d > s.r) ? 0.0 : sqrt(s.r * s.r - d * d);
+    Point nor = (l.a - l.b) / dist(l.a - l.b);
+    res.push_back(p + len * nor);
+    res.push_back(p - len * nor);
+  }
+  return res;
 }
