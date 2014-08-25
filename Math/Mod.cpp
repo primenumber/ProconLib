2242b3b15c7a47e57ac758a69298bf8cc5f78cf7
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-08-25 22:55:27 +0900

    Add mod

---
+++ Math/Mod.cpp
@@ -0,0 +1,20 @@
+const int mod = 1000000007;
+
+struct Mod {
+  int num;
+  Mod () : num(0) {;}
+  Mod (int n) : num(n) {;}
+  operator int() { return num; }
+};
+
+Mod operator+(Mod a, Mod b) { return Mod((a.num + b.num) % mod); }
+Mod operator-(Mod a, Mod b) { return Mod((mod + a.num - b.num) % mod); }
+Mod operator*(Mod a, Mod b) { return Mod(((long long)a.num * b.num) % mod); }
+Mod operator^(Mod a, int n) {
+  if (n == 0) return Mod(1);
+  Mod res = (a * a) ^ (n / 2);
+  if (n % 2) res = res * a;
+  return res;
+}
+Mod inv(Mod a) { return a ^ (mod - 2); }
+Mod operator/(Mod a, Mod b) { return a * inv(b); }
