1841c32b81c73c4362698a8d9bf2eeace4d52709
Takise <kazuki.takise@gmail.com>
2014-11-17 23:05:45 +0900

    Bug Fixed

--- Structure/FenwickTree.cpp
+++ Structure/FenwickTree.cpp
@@ -1,29 +1,4 @@
-struct Data {
-  int num;
-  Data() : num(0) {;}
-  Data(int n) : num(n) {;}
-};
-
-inline Data Merge(Data left, Data right) {
-  return Data left.num + right.num;
-}
-
-struct Fenwick {
-  static const int len = 1 << 18;
-  Data data[len];
-  Fenwick(void){ REP(i, len) data[i].num = 0; }
-  void update(int i, Data value) {
-    for (; i < len; i |= i+1) data[i] = Merge(dat[i], value);
-  }
-  Data query(int i) {
-    Data s = 0;
-    for (; i >= 0; i = (i&i+1)-1) s = Merge(s, data[i]);
-    return s;
-  }
-};
-
 // Light version
-
 typedef int Data;
 const int len = 1 << 18;
 
@@ -40,8 +15,31 @@ struct BIT {
   }
 };
 
-// Range Add, Range Sum
+struct Data {
+  int num;
+  Data() : num(0) {;}
+  Data(int n) : num(n) {;}
+};
 
+inline Data Merge(Data left, Data right) {
+  return Data(left.num + right.num);
+}
+
+struct BIT {
+  static const int len = 1 << 18;
+  Data data[len];
+  BIT(void){ REP(i, len) data[i].num = 0; }
+  void update(int i, Data value) {
+    for (; i < len; i |= i+1) data[i] = Merge(data[i], value);
+  }
+  Data query(int i) {
+    Data s = 0;
+    for (; i >= 0; i = (i&(i+1))-1) s = Merge(s, data[i]);
+    return s;
+  }
+};
+
+// Range Add, Range Sum
 struct RARS {
   BIT bit1, bit2;
   void add(int fr, int to, Data val) {
@@ -51,8 +49,8 @@ struct RARS {
     bit2.update(to, Data(-val.num));
   }
   Data sum(int fr, int to) {
-    Data s1 = bit1.query(to) + bit2.query(to) * to;
-    Data s2 = bit1.query(fr) + bit2.query(fr) * fr;
+    Data s1 = bit1.query(to).num + bit2.query(to).num * to;
+    Data s2 = bit1.query(fr).num + bit2.query(fr).num * fr;
     return Data(s1.num - s2.num);
   }
 };
