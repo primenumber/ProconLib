9602ef92e04ac0552d4f4f48363de1f52c8212be
primenumber <primenumber.proj@gmail.com>
2014-12-16 00:33:09 +0900

    add SkewHeapDijkstra

---
+++ Structure/SkewHeapDijkstra.cpp
@@ -0,0 +1,53 @@
+typedef int Weight;
+typedef pair<Weight,int> Data;
+
+Weight INF = 1000000000;
+struct Edge{
+  int src, dest; Weight weight;
+  bool operator < (const Edge &rhs) const {return weight > rhs.weight;}
+};
+
+typedef vector<Edge> Edges;
+typedef vector<Edges> Graph;
+typedef vector<Weight> Array;
+typedef vector<Array> Matrix;
+
+void add_edge(Graph &g, int src, int dest, Weight weight) {
+  g[src].push_back((Edge){src, dest, weight});
+  g[dest].push_back((Edge){dest, src, weight});
+}
+
+//O(V)-space dijkstra
+//G: function object
+template<typename G>
+void dijkstra(G &g, Array &d, int s) {
+  int n=g.size();
+  d.assign(n, INF);
+  d[s] = 0;
+  shared_ptr<Heap> que;
+  vector<shared_ptr<Heap>> nodes(n);
+  auto p = push(que, Data(0, s));
+  que = p.first;
+  nodes[s] = p.second;
+  assert(que!=nullptr);
+  while (que) {
+    Weight dist = que->top().first;
+    int v = que->top().second;
+    que = pop(que);
+    nodes[v].reset();
+    if (d[v] < dist) continue;
+    REP(i, g(v).size()) {
+      Edge e = g(v)[i];
+      if (d[e.dest] > d[v] + e.weight) {
+        d[e.dest] = d[v] + e.weight;
+        if (!nodes[e.dest]) {
+          auto p = push(que, Data(d[e.dest], e.dest));
+          que = p.first;
+          nodes[e.dest] = p.second;
+        } else {
+          que = reduce(nodes[e.dest], que, Data(d[e.dest], e.dest));
+        }
+      }
+    }
+  }
+}
