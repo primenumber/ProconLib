32831ec3057cadd5e988b94fc7388b050abb9ad4
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-03-11 15:43:36 +0900

    スライド最小値を追加

---
+++ Structure/SlideMin.cpp
@@ -0,0 +1,14 @@
+typedef int Data;
+
+struct SlideMin {
+  deque<pair<int,Data>> deq;
+  int c, w;
+  SlideMin(int w) : c(0), w(w) {;}
+  Data query(Data d) {
+    ++c;
+    while (!deq.empty() && deq.front().first <= c - w) deq.pop_front();
+    while (!deq.empty() && deq.back().second >= d) deq.pop_back();
+    deq.push_back(make_pair(c, d));
+    return deq.front().second;
+  }
+};
