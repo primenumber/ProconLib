d8418c9ea88c671dc33a6b6ca08052d469619f91
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-07-24 11:15:56 +0900

    Mod combination

--- Math/Mod.cpp
+++ Math/Mod.cpp
@@ -3,7 +3,7 @@ const int mod = 1000000007;
 struct Mod {
   int num;
   Mod () : num(0) {;}
-  Mod (int n) : num(n) {;}
+  Mod (int n) : num((n % mod + mod) % mod) {;}
   operator int() { return num; }
 };
 
@@ -12,6 +12,7 @@ Mod operator-(Mod a, Mod b) { return Mod((mod + a.num - b.num) % mod); }
 Mod operator*(Mod a, Mod b) { return Mod(((long long)a.num * b.num) % mod); }
 Mod operator+=(Mod &a, Mod b) { return a = a + b; }
 Mod operator-=(Mod &a, Mod b) { return a = a - b; }
+Mod operator*=(Mod &a, Mod b) { return a = a * b; }
 Mod operator^(Mod a, int n) {
   if (n == 0) return Mod(1);
   Mod res = (a * a) ^ (n / 2);
@@ -20,3 +21,17 @@ Mod operator^(Mod a, int n) {
 }
 Mod inv(Mod a) { return a ^ (mod - 2); }
 Mod operator/(Mod a, Mod b) { return a * inv(b); }
+
+#define MAX_N 1024000
+
+Mod fact[MAX_N], factinv[MAX_N];
+void init() {
+  fact[0] = Mod(1); factinv[0] = 1;
+  REP(i,MAX_N-1) {
+    fact[i+1] = fact[i] * Mod(i+1);
+    factinv[i+1] = factinv[i] / Mod(i+1);
+  }
+}
+Mod comb(int a, int b) {
+  return fact[a] * factinv[b] * factinv[a-b];
+}
