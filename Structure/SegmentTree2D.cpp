322b30cf3abd0beb5289265f70ca39858c65a366
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-08-13 21:37:09 +0900

    fix bug

--- Structure/SegmentTree2D.cpp
+++ Structure/SegmentTree2D.cpp
@@ -1,14 +1,12 @@
 struct Seg2D{
-  static const int STsize = 1 << 10;
-  int n;
-  SegmentTree data[STsize];
-  Seg2D() : n(STsize / 2) {}
+  int n; vector<SegmentTree> data;
+  Seg2D(int H, int W) : n(1 << H), data(n * 2, SegmentTree(W)) {}
   void update (int posi, int posj, Data value) {
     data[posi].update(posj, value);
     while (posi < 2*n-1) {
       int l = posi, r = posi^1;
       posi = posi / 2 + n;
-      data[posi].update(posj, Merge(data[posi].data[l], data[posi].data[r]));
+      data[posi].update(posj, Merge(data[l].data[posj], data[r].data[posj]));
     }
   }
   Data sub(int T, int B, int L, int R, int node, int la, int ra){
