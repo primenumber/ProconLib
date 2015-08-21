c0de6ab4ae618c8c03b49f5c0fc575db2306c9c5
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-08-21 22:55:51 +0900

    Bipartite Matching をデータ構造に書き換えた

--- Graph/BipartiteMatching.cpp
+++ Graph/BipartiteMatching.cpp
@@ -1,36 +1,34 @@
-#define MAX_V 1000000
-int V;
-
-vector<int> g[MAX_V];
-int match[MAX_V];
-bool used[MAX_V];
-
-void add_edge(int u, int v) {
-  g[u].pb(v);
-  g[v].pb(u);
-}
-
-bool dfs(int v) {
-  used[v] = true;
-  REP(i, g[v].size()) {
-    int u = g[v][i], w = match[u];
-    if (w < 0 || (!used[w] && dfs(w))) {
-      match[v] = u;
-      match[u] = v;
-      return true;
+class BipartiteMatching {
+  int V;
+  vector<vector<int>> g;
+  vector<int> match;
+  vector<bool> used;
+  bool dfs(int v) {
+    used[v] = true;
+    for (int u: g[v]) {
+      int w = match[u];
+      if (w < 0 || (!used[w] && dfs(w))) {
+        match[v] = u; match[u] = v;
+        return true;
+      }
     }
+    return false;
   }
-  return false;
-}
-
-int bipartite_matching(void) {
-  int res = 0;
-  memset(match, -1, sizeof(match));
-  REP(v, V) {
-    if (match[v] < 0) {
-      memset(used, 0, sizeof(used));
-      if (dfs(v)) res++;
+public:
+  BipartiteMatching (int v) : V(v), g(v), match(v), used(v) {}
+  void add_edge(int u, int v) {
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  int maximum_matching(void) {
+    int res = 0;
+    fill (ALL(match), -1);
+    REP(v, V) {
+      if (match[v] < 0) {
+        fill (ALL(used), 0);
+        if (dfs(v)) res++;
+      }
     }
+    return res;
   }
-  return res;
-}
+};
