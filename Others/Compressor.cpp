f09ed8ae2579a63b64fda4110238191bf73fb56f
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-08-21 22:55:05 +0900

    cosmetic

--- Others/Compressor.cpp
+++ Others/Compressor.cpp
@@ -1,11 +1,10 @@
 typedef int Data;
 vector<int> compressor(vector<Data> c) {
-  int n = c.size();
-  vector<Data> v(2 * n), w;
+  const int n = c.size();
+  vector<Data> v = c;
   vector<int> res(n);
-  REP(i,n) v[2*i] = c[i], v[2*i+1] = c[i] + 1;
   sort(v.begin(), v.end());
-  REP(i,v.size()) if (i == 0 || v[i] != v[i-1]) w.push_back(v[i]);
-  REP(i,n) res[i] = lower_bound(w.begin(), w.end(), c[i]) - w.begin();
+  auto it = unique(ALL(v));
+  REP(i,n) res[i] = lower_bound(begin(v), it, c[i]) - begin(v);
   return res;
 }
