9debbf9e079fc49f78d14a2dd4f85b7313de850a
primenumber <prime@kmc.gr.jp>
2021-06-13 20:49:48 +0900

    Template based SegmentTree

--- Structure/SegmentTree.cpp
+++ Structure/SegmentTree.cpp
@@ -1,25 +1,27 @@
 // verified: AOJ DSL_2 A
 
-struct Data {
+struct Monoid {
   int num;
-  Data() : num(0x7FFFFFFF) {;}
-  Data(int n) : num(n) {;}
+  Monoid() : num(0x7FFFFFFF) {;}
+  Monoid(int n) : num(n) {;}
 };
 
-inline Data Merge(Data left, Data right) {
-  return Data(left.num < right.num ? left.num : right.num);
-}
+const auto Merge = [](Monoid left, Monoid right) {
+  return Monoid(left.num < right.num ? left.num : right.num);
+};
 
+template <typename Data, typename F>
 struct SegmentTree {
   int n; vector<Data> data;
-  SegmentTree(int N) : n(1 << N), data(n * 2) {}
+  F merge;
+  SegmentTree(int N, F f) : n(1 << N), data(n * 2), merge(f) {}
   void update (int pos, Data value) {
     data[pos] = value;
     while (pos < 2*n-1) {
       int l = pos, r = pos^1;
       if (l > r) swap(l, r);
       pos = pos / 2 + n;
-      data[pos] = Merge(data[l], data[r]);
+      data[pos] = merge(data[l], data[r]);
     }
   }
   Data sub(int fr, int to, int node, int la, int ra) {
@@ -27,7 +29,7 @@ struct SegmentTree {
     if (fr<=la && ra<=to) return data[node];
     Data vl = sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
     Data vr = sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
-    return Merge(vl, vr);
+    return merge(vl, vr);
   }
   Data query(int fr, int to) { return sub(fr, to, 2*n-2, 0, n); }
 };
